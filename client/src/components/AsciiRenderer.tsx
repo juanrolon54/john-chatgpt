c3def873018cb975324731fb2b7081f312b14808
JohnCrossRollOn <juanrolon54@outlook.com>
2023-01-17 15:19:25 -0300

    typesafety issue

--- client/src/components/AsciiRenderer.tsx
+++ client/src/components/AsciiRenderer.tsx
@@ -12,10 +12,8 @@ export default function ({
     color = false,
     resolution = 0.15
 }) {
-    // Reactive state
     const { size, gl, scene, camera } = useThree()
 
-    // Create effect
     const effect = useMemo(() => {
         const effect = new AsciiEffect(gl, characters, { invert, color, resolution })
         effect.domElement.style.position = 'absolute'
@@ -25,31 +23,30 @@ export default function ({
         return effect
     }, [characters, invert, color, resolution])
 
-    // Styling
     useLayoutEffect(() => {
         effect.domElement.style.color = fgColor
         effect.domElement.style.backgroundColor = bgColor
     }, [fgColor, bgColor])
 
-    // Append on mount, remove on unmount
     useEffect(() => {
         gl.domElement.style.opacity = '0'
+        //@ts-ignore
         gl.domElement.parentNode.appendChild(effect.domElement)
         return () => {
             gl.domElement.style.opacity = '1'
+            //@ts-ignore
             gl.domElement.parentNode.removeChild(effect.domElement)
         }
     }, [effect])
 
-    // Set size
     useEffect(() => {
         effect.setSize(size.width, size.height)
     }, [effect, size])
 
-    // Take over render-loop (that is what the index is for)
     useFrame((state) => {
         effect.render(scene, camera)
     }, renderIndex)
 
     // This component returns nothing, it is a purely logical
+    return <></>
 }
